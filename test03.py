e7ae8a692f63f7331f10b79c2bdf1e1018d5b1a4
seokju <wooju0211@naver.com>
2024-09-26 11:08:06 +0900

    modify test03.py

--- test03.py
+++ test03.py
@@ -1,7 +1,9 @@
 #test03.py
 
 def sayHello(name):
-    return 'Hello, ' + name
+    return 'Hello, ' + name + '!!' # user01 수정
     
 inName = seokju
 print(sayHello(inName))
+
+print('Requested by boss') # user01 추가
